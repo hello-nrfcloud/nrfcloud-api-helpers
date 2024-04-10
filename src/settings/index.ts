58c88c976c1f75876c28d2a8e8454fc9d570dd3a
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-10 13:25:22 +0200

    fix: remove CLI code

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,6 +1,5 @@
 export * from './settings.js'
 export * from './scope.js'
-export * from './initializeAccount.js'
 export * from './getAllAccounts.js'
 export * from './getAllAccountsSettings.js'
 export * from './groupByAccount.js'
