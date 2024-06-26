52929cba3be53c62c1fff5d700d986d03dcdb1f4
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-06-26 15:17:12 +0200

    fix: update dependencies
    Closes #48

--- src/api/getFOTABundle.spec.ts
+++ src/api/getFOTABundle.spec.ts
@@ -4,52 +4,57 @@ import { getFOTABundles } from './getFOTABundles.js'
 
 void describe('getFOTABundles()', () => {
 	void it('should fetch all FOTA bundles', async () => {
-		const mockFetch = mock.fn()
+		const mockFetch = mock.fn<() => Promise<Response>>()
 		mockFetch.mock.mockImplementationOnce(
-			() => ({
-				ok: true,
-				json: async () =>
-					Promise.resolve({
-						items: [
-							{
-								bundleId: 'APP*0038b655*v1.1.1-debug',
-								lastModified: '2023-06-28T09:50:02.000Z',
-								size: 418565,
-								version: 'v1.1.1-debug',
-								type: 'APP',
-								filenames: ['hello-nrfcloud-thingy91-debug-v1.1.1-fwupd.bin'],
-								name: 'hello.nrfcloud.com v1.1.1-debug',
-								description: 'Firmware Update Image BIN file (thingy91, debug)',
-							},
-						],
-						total: 1,
-						pageNextToken:
-							'102/B5fGZNAs7vcw8E2i611ID4apx/Du/2/H6nr2UDWk5eoihEeAgh6qoaGcDzAI4M8JCoO4iAAK96TWfuB19ru9c1PrnwiTUdw/sZzwrYSrS433vPjDJNvJUIEmqm9+V3ElM5M1bLmt6GrGa57SymHHK4nN0W+zHhmE97cCCfzJMBXhVTl3TzvBx5rE1KJYf',
-					}),
-			}),
+			async () =>
+				Promise.resolve({
+					ok: true,
+					json: async () =>
+						Promise.resolve({
+							items: [
+								{
+									bundleId: 'APP*0038b655*v1.1.1-debug',
+									lastModified: '2023-06-28T09:50:02.000Z',
+									size: 418565,
+									version: 'v1.1.1-debug',
+									type: 'APP',
+									filenames: ['hello-nrfcloud-thingy91-debug-v1.1.1-fwupd.bin'],
+									name: 'hello.nrfcloud.com v1.1.1-debug',
+									description:
+										'Firmware Update Image BIN file (thingy91, debug)',
+								},
+							],
+							total: 1,
+							pageNextToken:
+								'102/B5fGZNAs7vcw8E2i611ID4apx/Du/2/H6nr2UDWk5eoihEeAgh6qoaGcDzAI4M8JCoO4iAAK96TWfuB19ru9c1PrnwiTUdw/sZzwrYSrS433vPjDJNvJUIEmqm9+V3ElM5M1bLmt6GrGa57SymHHK4nN0W+zHhmE97cCCfzJMBXhVTl3TzvBx5rE1KJYf',
+						}),
+				}) as Promise<Response>,
 			0,
 		)
 		mockFetch.mock.mockImplementationOnce(
-			() => ({
-				ok: true,
-				json: async () =>
-					Promise.resolve({
-						items: [
-							{
-								bundleId: 'APP*0103b0f9*v1.1.2-sol-dbg',
-								lastModified: '2023-06-29T14:18:19.000Z',
-								size: 426280,
-								version: 'v1.1.2-sol-dbg',
-								type: 'APP',
-								filenames: ['hello-nrfcloud-thingy91-sol-dbg-v1.1.2-fwupd.bin'],
-								name: 'hello.nrfcloud.com v1.1.2-sol-dbg',
-								description:
-									'Firmware Update Image BIN file (thingy91, solar, debug)',
-							},
-						],
-						total: 1,
-					}),
-			}),
+			async () =>
+				Promise.resolve({
+					ok: true,
+					json: async () =>
+						Promise.resolve({
+							items: [
+								{
+									bundleId: 'APP*0103b0f9*v1.1.2-sol-dbg',
+									lastModified: '2023-06-29T14:18:19.000Z',
+									size: 426280,
+									version: 'v1.1.2-sol-dbg',
+									type: 'APP',
+									filenames: [
+										'hello-nrfcloud-thingy91-sol-dbg-v1.1.2-fwupd.bin',
+									],
+									name: 'hello.nrfcloud.com v1.1.2-sol-dbg',
+									description:
+										'Firmware Update Image BIN file (thingy91, solar, debug)',
+								},
+							],
+							total: 1,
+						}),
+				}) as Promise<Response>,
 			1,
 		)
 		const fetcher = getFOTABundles(
