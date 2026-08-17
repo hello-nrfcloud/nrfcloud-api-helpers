0dd0165e5db19688ed6e0f6f031682f32eac0939
Markus Tacker <Markus.Tacker@NordicSemi.no>
2026-08-17 13:24:37 +0200

    fix: use native compiler

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,2 @@
 import config from '@bifravst/eslint-config-typescript'
-export default [...config, { ignores: ['npm/**'] }, { files: ['./.npm/*.ts'] }]
+export default [...config, { ignores: ['npm/**'] }]
